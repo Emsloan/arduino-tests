39117df0699370c414522110fc9b6680551c59e6
Emsloan <55118249+Emsloan@users.noreply.github.com>
2022-01-18 09:08:06 -0700

    Create task178.py

---
+++ emsloan/task178.py
@@ -0,0 +1,3 @@
+
+# The following function call is used to write commands to the CMD.
+subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
