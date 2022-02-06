40f13502bd581820f3c0b83cd3d400cadeed11ae
Jemimah Thomas <jthoma74@asu.edu>
2022-02-05 20:52:02 -0500

    #206 Testing

---
+++ jthoma74_Mac_Tests/Apple_Intel/206_Sketch.X/build/default/production/newavr-main1.o.d
@@ -0,0 +1,44 @@
+build/default/production/newavr-main1.o.d \
+ build/default/production/newavr-main1.o: newavr-main1.c \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/io.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/sfr_defs.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/inttypes.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/features.h \
+ /Applications/microchip/xc8/v2.35/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/stdint.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/bits/alltypes.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/bits/stdint.h \
+ /Applications/microchip/mplabx/v5.50/packs/Microchip/AVR-Dx_DFP/1.8.112/xc8/avr/include/avr/ioavr128da48.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/portpins.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/common.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/xmega.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/fuse.h \
+ /Applications/microchip/xc8/v2.35/avr/avr/include/avr/lock.h
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/io.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/sfr_defs.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/inttypes.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/features.h:
+
+/Applications/microchip/xc8/v2.35/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/stdint.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/bits/alltypes.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/bits/stdint.h:
+
+/Applications/microchip/mplabx/v5.50/packs/Microchip/AVR-Dx_DFP/1.8.112/xc8/avr/include/avr/ioavr128da48.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/portpins.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/common.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/xmega.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/fuse.h:
+
+/Applications/microchip/xc8/v2.35/avr/avr/include/avr/lock.h:
---
+++ jthoma74_Mac_Tests/Apple_Intel/206_Sketch.X/newavr-main1.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newavr-main1.c
+ * Author: jembox
+ *
+ * Created on February 5, 2022, 8:46 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
