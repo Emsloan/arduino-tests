b6c2d53acc3277f494e63c34fe93e1923cb62b6c
Jemimah Thomas <jthoma74@asu.edu>
2022-03-02 22:38:22 -0800

    #257 PinTests

---
+++ jthoma74_Mac_Tests/Apple_Intel/PinTests/PinTest257/PinTest257/PinTest257.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+/**
+ * Testing pin maps using Analog read input on Arduino
+ */
+
+int outputPin = 33;  // pin to test
+int inputPin = 2;    // input pin to read from output pin
+int val = 0;      // variable to store read value
+
+void setup() {
+  Serial1.begin(9600);
+  pinMode(outputPin, OUTPUT);  // sets pin as output
+  pinMode(inputPin, INPUT);    // sets pin as input
+}
+
+void loop() {
+  digitalWrite(outputPin,HIGH);
+  delay(1000);
+  val = digitalRead(inputPin);   // read the input pin
+  Serial1.print("Sent signal from: ");
+  Serial1.println(outputPin);
+  Serial1.print("Received signal: ");
+  Serial1.println(val);
+  delay(1);        // delay in between reads for stability
+/*
+  // reply only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    val = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(val, DEC);
+  }
+  */
+}
