1c81c9b07ce2b74c022d2c014b15034a7bded51c
Paul Duncanson <pduncans@asu.edu>
2022-04-20 10:13:39 -0400

    PDF versions of markdowns added

--- pduncans/244/MPU6050_accelerometer_test/MPU6050_accelerometer_test.ino
+++ pduncans/244/MPU6050_accelerometer_test/MPU6050_accelerometer_test.ino
@@ -16,13 +16,13 @@ MPU6050 mpu;
 
 void setup() 
 {
-  Serial3.begin(115200);
+  Serial1.begin(115200);
 
-  Serial3.println("Initialize MPU6050");
+  Serial1.println("Initialize MPU6050");
 
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
-    Serial3.println("Could not find a valid MPU6050 sensor, check wiring!");
+    Serial1.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
 }
@@ -33,12 +33,12 @@ void loop()
   Vector rawAccel = mpu.readRawAccel();
   Vector normAccel = mpu.readNormalizeAccel();
 
-  Serial3.print(" Xraw = ");
-  Serial3.print(rawAccel.XAxis);
-  Serial3.print(" Yraw = ");
-  Serial3.print(rawAccel.YAxis);
-  Serial3.print(" Zraw = ");
-  Serial3.println(rawAccel.ZAxis);
+  Serial1.print(" Xraw = ");
+  Serial1.print(rawAccel.XAxis);
+  Serial1.print(" Yraw = ");
+  Serial1.print(rawAccel.YAxis);
+  Serial1.print(" Zraw = ");
+  Serial1.println(rawAccel.ZAxis);
  
  // Slow down the scrolling for readability
   delay(1000);
